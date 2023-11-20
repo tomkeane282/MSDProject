aa4aeb36785c5a9cfba49f3df89c0b684338f14f
TOMKE <tomkeane282@gmail.com>
2023-11-20 22:03:48 +0000

    Basic demo of 3 activities. Linked with buttons on home page.

---
+++ app/src/main/java/com/example/msdproject/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.msdproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button Movie1Button = findViewById(R.id.Movie1Button);
+        Movie1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Movie1.class);
+                startActivity(intent);
+            }
+        });
+
+        Button profileButton = findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Profile.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
